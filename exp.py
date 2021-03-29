f8273150e8d5eb44c6115851ef2925d4cefdafc5
Victor Gallego <vicgalle@ucm.es>
2021-03-29 23:31:37 +0200

    update dir

--- exp.py
+++ exp.py
@@ -4,25 +4,29 @@ from PIL import Image
 import glob
 import numpy as np
 
+#DIR = "/home/victor/wikiart"
+#DIR = "/home/victor/ukiyo-e/img"
+DIR = "/home/victor/ukiyo-e/img_met_large"
+
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
 
 logs = []
-N = len(glob.glob("/home/victor/wikiart/*/*"))
-#N = 3000
+N = len(glob.glob(f"{DIR}/*/*"))
+print(N)
 
-if False:
+if True:
 
-    for i, name in enumerate(glob.glob("/home/victor/wikiart/*/*")[:N]):
+    for i, name in enumerate(glob.glob(f"{DIR}/*/*")[:N]):
         print(i, name)
         image = preprocess(Image.open(name)).unsqueeze(0).to(device)
         logits_image = model.encode_image(image)
         logs.append(logits_image.detach().numpy())
 
     image_features = torch.cat([torch.tensor(log) for log in logs], dim=0)
-    torch.save(image_features, '/home/victor/wikiart/image_features.pt')
+    torch.save(image_features, f'{DIR}/image_features.pt')
 
-image_featuresl = torch.load('/home/victor/wikiart/image_features.pt')
+image_featuresl = torch.load(f'{DIR}/image_features.pt')
 
 
 print(image_featuresl.shape)
@@ -38,6 +42,6 @@ similarity = (100.0 * image_features @ text_features.T).softmax(dim=0)
 print(similarity.shape)
 values, indices = similarity[:, 0].topk(5)
 
-names = np.asarray(glob.glob("/home/victor/wikiart/*/*")[:N])
+names = np.asarray(glob.glob(f"{DIR}/*/*")[:N])
 print(values)
 print(names[indices.numpy()])
