f28beea3e5bade66102eed2091f75eb2428a04b6
Victor Gallego <vicgalle@ucm.es>
2021-04-02 14:15:44 +0200

    add image analogies

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ def main():
     """This demo demonstrates the use of zero-shot learning for returning the closest painting for a given text query."""
 
     # Read in models from the data files.
-    model, image_featuresl = load_model()
+    model, preprocess, image_featuresl = load_model()
     device = "cuda" if torch.cuda.is_available() else "cpu"
     st.sidebar.title('Info')
 
@@ -24,6 +24,7 @@ def main():
         """Developed by @vicgalle.
         """
     )
+    
     ex_names = [
         'a painting of a woman in a green dress',
         'a painting of two persons kissing on the bed',
@@ -82,6 +83,39 @@ def main():
     st.write('These are first four results, only one of them satisfies the query:')
     st.image(image, use_column_width=True)
 
+    st.header("The Great Image Analogizer!")
+
+    img_file_buffer = st.file_uploader("Upload an image as your query", type=['png', 'jpg', 'jpeg'])
+    if img_file_buffer is not None:
+        image = Image.open(img_file_buffer)
+
+        st.image(
+            image,
+            caption=f"You amazing query image!",
+            width = 500
+        )
+
+        image = preprocess(image).unsqueeze(0).to(device)
+        query_features = model.encode_image(image)
+        query_features = query_features/query_features.norm(dim=-1, keepdim=True)
+
+        similarity = (100.0 * image_features.float() @ query_features.float().T).softmax(dim=0)
+
+        values, indices = similarity[:, 0].topk(3)
+
+        names = np.asarray(glob.glob("wikiart/*/*"))
+        print(values)
+        print(names[indices.cpu().numpy()])
+
+        st.subheader("Top 3 closest paintings to your query:")
+        
+        for i in range(3):
+            image = Image.open(names[indices.cpu().numpy()][i])
+            st.image(image, width = 500)
+        
+
+
+
 
 @st.cache(allow_output_mutation=True)
 def load_model():
@@ -93,7 +127,7 @@ def load_model():
     model, preprocess = clip.load("ViT-B/32", device=device)
     image_featuresl = torch.load('wikiart/image_features.pt')
 
-    return model, image_featuresl
+    return model, preprocess, image_featuresl
 
 
 if __name__ == "__main__":
